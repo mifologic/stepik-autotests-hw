172da5644e5befd1ebae7ac11b6312f1bb549556
Mariana Piskareva <mifologic@gmail.com>
2020-02-23 18:14:58 +0300

    update conftest file for run test with options in command line

--- Module3/conftest.py
+++ Module3/conftest.py
@@ -1,11 +1,24 @@
 import pytest
 from selenium import webdriver
 
+# Добавляем опцию, чтобы передавать параметры для запуска в командной строке
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome",
+                      help="Choose browser: chrome or firefox")
 
-@pytest.fixture
-def driver():
-    print("\ncreate driver for test")
-    driver = webdriver.Chrome()
-    yield driver
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
     print("\nclose browser after test")
-    driver.quit()
+    browser.quit()
--- Module3/test_parametrization.py
+++ Module3/test_parametrization.py
@@ -14,14 +14,14 @@ from selenium.webdriver.support import expected_conditions as EC
                                   "https://stepik.org/lesson/236903/step/1",
                                   "https://stepik.org/lesson/236904/step/1",
                                   "https://stepik.org/lesson/236905/step/1"])
-def test_parametrization_feedback(driver, link):
+def test_parametrization_feedback(browser, link):
 
     answer = math.log(int(time.time()))
 
-    driver.get(link)
-    textarea = WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".textarea")))
+    browser.get(link)
+    textarea = WebDriverWait(browser, 7).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".textarea")))
     textarea.send_keys(str(answer))
-    driver.find_element(By.CSS_SELECTOR, ".submit-submission").click()
-    message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".smart-hints__hint")))
+    browser.find_element(By.CSS_SELECTOR, ".submit-submission").click()
+    message = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".smart-hints__hint")))
     text = message.text
     assert text == "Correct!"
