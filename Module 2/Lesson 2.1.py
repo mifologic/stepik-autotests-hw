615bb5bb1946694d3d6c5ceb89ac259aa84bf6b0
Mariana Piskareva <mifologic@gmail.com>
2020-02-23 16:15:37 +0300

    Добавлен файл с заданием из урока 2.1

---
+++ Module 2/Lesson 2.1.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+driver = webdriver.Chrome()
+driver.get("http://suninjuly.github.io/math.html")
+x = driver.find_element(By.ID, "input_value").text
+print(x)
+y = calc(x)
+field = driver.find_element(By.ID, "answer")
+field.send_keys(y)
+driver.find_element(By.ID, "robotCheckbox").click()
+driver.find_element(By.ID, "robotsRule").click()
+driver.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
